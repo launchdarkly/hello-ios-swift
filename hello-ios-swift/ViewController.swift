d96c88e6540eed1b2c60bb47a1abdfa048748de1
Korhan <korhan.bircan@capitalone.com>
2017-04-08 18:59:48 -0700

    Created the Xcode single-view project.

---
+++ hello-ios-swift/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  LaunchDarklyHelloWorld
+//
+//  Created by Korhan Bircan on 3/24/17.
+//  Copyright © 2017 Korhan Bircan. All rights reserved.
+//
+
+import UIKit
+import LaunchDarkly
+
+class ViewController: UIViewController {
+    @IBOutlet weak var featureFlagLabel: UILabel!
+
+    // Enter your feature flag name here.
+    fileprivate let featureFlagKey = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        LDClient.sharedInstance().delegate = self
+        checkFeatureValue()
+    }
+
+    fileprivate func checkFeatureValue() {
+        let featureFlagValue = LDClient.sharedInstance().boolVariation(featureFlagKey, fallback: false)
+        updateLabel(value: featureFlagValue)
+    }
+
+    fileprivate func updateLabel(value: Bool) {
+        featureFlagLabel.text = "\(featureFlagKey): \(value)"
+    }
+}
+
+extension ViewController: ClientDelegate {
+    func featureFlagDidUpdate(_ key: String!) {
+        if key == featureFlagKey {
+            checkFeatureValue()
+        }
+    }
+}
